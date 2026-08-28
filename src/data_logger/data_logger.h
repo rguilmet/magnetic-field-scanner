53525d1cddc8c81db6c3548b9e18e5089889dbea
rguilmet <rguilmet@users.noreply.github.com>
2026-08-28 17:07:36 -0400

    Fix compilation error: Update log_data prototype in data_logger.h

--- src/data_logger/data_logger.h
+++ src/data_logger/data_logger.h
@@ -10,7 +10,8 @@ void start_logging(void);
 void stop_logging(void);
 void start_calibration_logging(void);
 bool process_calibration_file(void);
-void log_data(uint32_t timestamp, float gain, int cc, int32_t refX_raw, int32_t refY_raw, int32_t refZ_raw, int32_t tipX_raw, int32_t tipY_raw, int32_t tipZ_raw, int32_t refX_cal, int32_t refY_cal, int32_t refZ_cal, int32_t tipX_cal, int32_t tipY_cal, int32_t tipZ_cal, int32_t calOffsetX, int32_t calOffsetY, int32_t calOffsetZ, int32_t gradX, int32_t gradY, int32_t gradZ, float mag, float nT, float accX, float accY, float accZ, float gyrX, float gyrY, float gyrZ, float freq, bool is_muted);
+void log_data(uint32_t timestamp, float gain, int cc, int32_t refX_raw, int32_t refY_raw, int32_t refZ_raw, int32_t tipX_raw, int32_t tipY_raw, int32_t tipZ_raw, int32_t refX_cal, int32_t refY_cal, int32_t refZ_cal, int32_t tipX_cal, int32_t tipY_cal, int32_t tipZ_cal, int32_t calOffsetX, int32_t calOffsetY, int32_t calOffsetZ, int32_t gradX, int32_t gradY, int32_t gradZ, float mag, float nT, float accX, float accY, float accZ, float gyrX, float gyrY, float gyrZ, float freq, bool is_muted, float qw, float qx, float qy, float qz);
 #ifdef __cplusplus
 }
 #endif
+
